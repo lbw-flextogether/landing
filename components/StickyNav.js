21f43dafaaa90a2480118b3dfa8fc04ed0a3c629
omar okla <omaro@me.com>
2019-03-12 21:20:05 -0400

    defined nav

--- components/StickyNav.js
+++ components/StickyNav.js
@@ -1,3 +1,9 @@
 // Here I will create my code to have a nav bar that will 
 // follow me when scrolling!
 
+
+//Define my nav bar
+let navBar = document.querySelector(".navBar");
+console.log(navBar)
+
+//
