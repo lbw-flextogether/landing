7b1a4d285d4cf55ee4f659424f2969d922d9011a
omar okla <omaro@me.com>
2019-03-12 22:51:03 -0400

    Finished StickyNav

--- components/StickyNav.js
+++ components/StickyNav.js
@@ -1,26 +1,30 @@
 // Here I will create my code to have a nav bar that will 
 // follow me when scrolling!
 
-
 //Define my nav bar
-let navBar = document.querySelector(".navBar");
-console.log(navBar)
+let navBar = document.querySelector(".navContainer");
 
 //calculating position
-
 let sticky = navBar.offsetTop;
-console.log(sticky)
 
 //function declaration
 
 function scrollEvent() {
-    if (window.pageYOffset >= sticky) {
+    if (window.pageYOffset > sticky) {
         navBar.classList.add("sticky")
+        console.log("time to stick")
     }
     else {
         navBar.classList.remove("sticky")
+        console.log("unstick")
     }
+    // console.log('hi')
 }
 
 //listening and invokation!
+window.onscroll = function() {scrollEvent()};
 
+
+
+////// Let's expand this and make our navBar have functioning anchors
+/// With active anchor effects!
